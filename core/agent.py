e6c0b4f8bffef8ce572e3ee4b20606780795a27d
Lucas Willems <lcswillems@gmail.com>
2018-04-12 01:10:54 -0400

    Simplification du code

--- core/agent.py
+++ core/agent.py
@@ -5,67 +5,50 @@ import torch
 from torch.autograd import Variable
 import math
 import time
+import numpy as np
 
 
-def collect_samples(pid, queue, env, policy, custom_reward,
-                    render, update_rs, min_batch_size):
+def collect_samples(pid, queue, env, policy, render, min_batch_size):
     torch.randn(pid, )
     log = dict()
     memory = Memory()
     num_steps = 0
     total_reward = 0
-    min_reward = 1e6
-    max_reward = -1e6
-    total_c_reward = 0
-    min_c_reward = 1e6
-    max_c_reward = -1e6
+    min_reward = float('+inf')
+    max_reward = float('-inf')
     num_episodes = 0
 
+    obs = env.reset()
+    reward_episode = 0
+
     while num_steps < min_batch_size:
-        obs = env.reset()
-        reward_episode = 0
+        obs_var = Variable(torch.from_numpy(obs).float().unsqueeze(0), volatile=True)
+        action = policy.select_action(obs_var)[0].numpy()
+        next_obs, reward, done, _ = env.step(action)
+        mask = 0 if done else 1
+        memory.push(obs, action, next_obs, reward, mask)
+        obs = next_obs            
 
-        for t in range(10000):
-            obs_var = Variable(torch.from_numpy(obs).float().unsqueeze(0), volatile=True)
-            action = policy.select_action(obs_var)[0].numpy()
-            next_obs, reward, done, _ = env.step(action)
-            reward_episode += reward
+        if render:
+            env.render()
 
-            if custom_reward is not None:
-                reward = custom_reward(obs, action)
-                total_c_reward += reward
-                min_c_reward = min(min_c_reward, reward)
-                max_c_reward = max(max_c_reward, reward)
+        reward_episode += reward
+        num_steps += 1
+        
+        if done:
+            num_episodes += 1
+            min_reward = min(min_reward, reward_episode)
+            max_reward = max(max_reward, reward_episode)
+            total_reward += reward_episode
+            log['num_steps'] = num_steps
+            log['num_episodes'] = num_episodes
+            log['total_reward'] = total_reward
+            log['avg_reward'] = total_reward / num_episodes
+            log['max_reward'] = max_reward
+            log['min_reward'] = min_reward
 
-            mask = 0 if done else 1
-
-            memory.push(obs, action, mask, next_obs, reward)
-
-            if render:
-                env.render()
-            if done:
-                break
-
-            obs = next_obs
-
-        # log stats
-        num_steps += (t + 1)
-        num_episodes += 1
-        total_reward += reward_episode
-        min_reward = min(min_reward, reward_episode)
-        max_reward = max(max_reward, reward_episode)
-
-    log['num_steps'] = num_steps
-    log['num_episodes'] = num_episodes
-    log['total_reward'] = total_reward
-    log['avg_reward'] = total_reward / num_episodes
-    log['max_reward'] = max_reward
-    log['min_reward'] = min_reward
-    if custom_reward is not None:
-        log['total_c_reward'] = total_c_reward
-        log['avg_c_reward'] = total_c_reward / num_steps
-        log['max_c_reward'] = max_c_reward
-        log['min_c_reward'] = min_c_reward
+            obs = env.reset()
+            reward_episode = 0
 
     if queue is not None:
         queue.put([pid, memory, log])
@@ -81,20 +64,13 @@ def merge_log(log_list):
     log['avg_reward'] = log['total_reward'] / log['num_episodes']
     log['max_reward'] = max([x['max_reward'] for x in log_list])
     log['min_reward'] = min([x['min_reward'] for x in log_list])
-    if 'total_c_reward' in log_list[0]:
-        log['total_c_reward'] = sum([x['total_c_reward'] for x in log_list])
-        log['avg_c_reward'] = log['total_c_reward'] / log['num_steps']
-        log['max_c_reward'] = max([x['max_c_reward'] for x in log_list])
-        log['min_c_reward'] = min([x['min_c_reward'] for x in log_list])
-
     return log
 
 
 class Agent:
-    def __init__(self, env_factory, policy, custom_reward=None, render=False, num_threads=1):
+    def __init__(self, env_factory, policy, render=False, num_threads=1):
         self.env_factory = env_factory
         self.policy = policy
-        self.custom_reward = custom_reward
         self.render = render
         self.num_threads = num_threads
         self.env_list = []
@@ -110,14 +86,14 @@ class Agent:
         workers = []
 
         for i in range(self.num_threads-1):
-            worker_args = (i+1, queue, self.env_list[i + 1], self.policy, self.custom_reward,
-                           False, False, thread_batch_size)
+            worker_args = (i+1, queue, self.env_list[i + 1], self.policy,
+                           False, thread_batch_size)
             workers.append(multiprocessing.Process(target=collect_samples, args=worker_args))
         for worker in workers:
             worker.start()
 
-        memory, log = collect_samples(0, None, self.env_list[0], self.policy, self.custom_reward,
-                                      self.render, True, thread_batch_size)
+        memory, log = collect_samples(0, None, self.env_list[0], self.policy,
+                                      self.render, thread_batch_size)
 
         worker_logs = [None] * len(workers)
         worker_memories = [None] * len(workers)
