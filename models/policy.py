c8808f0b7cfc20fb8ac6ad2eff1e2fbac3073e7a
Lucas Willems <lcswillems@gmail.com>
2018-04-13 22:11:08 -0400

    - Name of last layer - Weight initialization of last layer of policy

--- models/policy.py
+++ models/policy.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-from models.utils import initialize_weights
+from models.utils import initialize_parameters
 
 class Policy(nn.Module):
     def __init__(self, obs_space, action_space):
@@ -11,16 +11,17 @@ class Policy(nn.Module):
 
         self.fc1 = nn.Linear(obs_space.shape[1], 128)
         self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, action_space.n)
+        self.policy_head = nn.Linear(128, action_space.n)
 
-        self.apply(initialize_weights)
+        self.policy_head.weight.data.mul_(0.1)
+        self.policy_head.bias.data.mul_(0.0)
 
     def forward(self, x):
         x = self.fc1(x)
         x = F.tanh(x)
         x = self.fc2(x)
         x = F.tanh(x)
-        x = self.fc3(x)
+        x = self.policy_head(x)
         return x
 
     def get_dist(self, x):
--- models/value.py
+++ models/value.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from models.utils import initialize_weights
+from models.utils import initialize_parameters
 
 class Value(nn.Module):
     def __init__(self, obs_space):
@@ -10,14 +10,12 @@ class Value(nn.Module):
 
         self.fc1 = nn.Linear(obs_space.shape[1], 128)
         self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, 1)
-
-        self.apply(initialize_weights)
+        self.value_head = nn.Linear(128, 1)
 
     def forward(self, x):
         x = self.fc1(x)
         x = F.tanh(x)
         x = self.fc2(x)
         x = F.tanh(x)
-        x = self.fc3(x)
+        x = self.value_head(x)
         return x
