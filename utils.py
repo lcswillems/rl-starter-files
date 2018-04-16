3f196bcdd1e0310208e8978cf4f1fb590614568b
Lucas Willems <lcswillems@gmail.com>
2018-04-16 00:38:00 -0400

    - Simplified utils functions - Use the recommanded way to save models

--- utils.py
+++ utils.py
@@ -1,10 +1,7 @@
 import os
 import torch
-import pickle
 
-import ac_rl
-from models.policy import Policy
-from models.value import Value
+from model import ACModel
 
 def storage_dir():
     return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'storage')
@@ -13,27 +10,13 @@ def get_model_path(model_name):
     return os.path.join(storage_dir(), 'models', model_name+".pt")
 
 def load_model(observation_space, action_space, from_path):
-    if from_path == None or not(os.path.exists(from_path)):
-        policy_net = Policy(observation_space, action_space)
-        value_net = Value(observation_space)
-    else:
-        policy_net, value_net = pickle.load(open(from_path, "rb"))
+    acmodel = ACModel(observation_space, action_space)
+    if from_path != None and os.path.exists(from_path):
+        acmodel.load_state_dict(torch.load(from_path))
+    return acmodel
 
-    if ac_rl.use_gpu:
-        policy_net = policy_net.cuda()
-        value_net = value_net.cuda()
-
-    return policy_net, value_net
-
-def save_model(policy_net, value_net, to_path):
-    if ac_rl.use_gpu:
-        policy_net.cpu(), value_net.cpu()
-    
+def save_model(acmodel, to_path):
     dirname = os.path.dirname(to_path)
-    if not(dirname):
+    if not(os.path.isdir(dirname)):
         os.makedirs(dirname)
-
-    pickle.dump((policy_net, value_net), open(to_path, 'wb'))
-
-    if ac_rl.use_gpu:
-        policy_net.cuda(), value_net.cuda()
+    torch.save(acmodel.state_dict(), to_path)
