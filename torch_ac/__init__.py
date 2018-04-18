6dfd62605ce4a3cf3ee06701490b33485e0fae69
Lucas Willems <lcswillems@gmail.com>
2018-04-18 19:46:04 -0400

    "default_preprocess_obss" and "default_shape_reward" should not be accessible outside the package

--- torch_ac/__init__.py
+++ torch_ac/__init__.py
@@ -1,4 +1,3 @@
 from torch_ac.algos import A2CAlgo, PPOAlgo
 from torch_ac.model import ACModel
-from torch_ac.format import default_preprocess_obss, default_reshape_reward
 from torch_ac.utils import use_gpu, seed
