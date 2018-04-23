d79c9ca254528a9748297402478ae0fc8ebd8799
Lucas Willems <lcswillems@gmail.com>
2018-04-23 18:04:29 -0400

    Don't open and close log file at every log

--- utils/log.py
+++ utils/log.py
@@ -12,6 +12,8 @@ def synthesize(array):
 class Logger:
     def __init__(self, log_name):
         self.path = get_log_path(log_name)
+        utils.create_folders_if_necessary(self.path)
+        self.file = open(self.path, "a")
     
     def log(self, obj, to_print=True):
         obj_str = str(obj)
@@ -19,6 +21,4 @@ class Logger:
         if to_print:
             print(obj_str)
 
-        utils.create_folders_if_necessary(self.path)
-        with open(self.path, "a") as f:
-            f.write(obj_str+"\n")
+        self.file.write(obj_str+"\n")
