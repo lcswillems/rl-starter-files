a890c5b6e2b121074c6ad1fd2a4f527b53da46df
Lucas Willems <lcswillems@gmail.com>
2018-04-23 17:54:00 -0400

    - Add support for instructions - Improved model

--- model.py
+++ model.py
@@ -17,43 +17,35 @@ class ACModel(torch_ac.ACModel):
         super().__init__()
 
         # Decide if the instruction is taken into account
-        self.forget_instr = True
+        self.use_instr = "instr" in obs_space.keys()
 
-        # Define sizes of some layers
-        self.instr_embedding_size = 10
-        self.obs_embedding_size = obs_space["image"]
-        if not(self.forget_instr):
-            self.obs_embedding_size += self.instr_embedding_size
-
-        # Define instruction embedding model
-        if not(self.forget_instr):
+        # Define instruction embedding
+        self.instr_embedding_size = 0
+        if self.use_instr:
+            self.instr_embedding_size = 128
             self.instr_gru = nn.GRU(obs_space["instr"], self.instr_embedding_size)
 
         # Define actor's model
-        self.a_fc1 = nn.Linear(self.obs_embedding_size, 128)
-        self.a_fc2 = nn.Linear(128, 128)
-        self.a_head = nn.Linear(128, action_space.n)
+        self.a_fc1 = nn.Linear(obs_space["image"] + self.instr_embedding_size, 64)
+        self.a_fc2 = nn.Linear(64, 64)
+        self.a_head = nn.Linear(64, action_space.n)
 
         # Define critic's model
-        self.c_fc1 = nn.Linear(self.obs_embedding_size, 128)
-        self.c_fc2 = nn.Linear(128, 128)
-        self.c_head = nn.Linear(128, 1)
+        self.c_fc1 = nn.Linear(obs_space["image"] + self.instr_embedding_size, 64)
+        self.c_fc2 = nn.Linear(64, 64)
+        self.c_head = nn.Linear(64, 1)
 
-        # Reduce values of critic's head layer for increasing entropy
-        self.a_head.weight.data.mul_(0.1)
-        self.a_head.bias.data.mul_(0.0)
+        # Initialize parameters correctly
+        self.apply(initialize_parameters)
 
     def get_embed_instr(self, instr):
         _, hidden = self.instr_gru(instr)
         return hidden[-1]
-    
-    def get_embed_obs(self, obs):
-        if self.forget_instr:
-            return obs["image"]
-        return torch.cat((obs["image"], self.get_embed_instr(obs["instr"])), dim=1)
 
     def get_rdist(self, obs):
-        x = self.get_embed_obs(obs)
+        x = obs["image"]
+        if self.use_instr:
+            x = torch.cat((x, self.get_embed_instr(obs["instr"])), dim=1)
         x = self.a_fc1(x)
         x = F.tanh(x)
         x = self.a_fc2(x)
@@ -61,7 +53,9 @@ class ACModel(torch_ac.ACModel):
         return self.a_head(x)
 
     def get_value(self, obs):
-        x = self.get_embed_obs(obs)
+        x = obs["image"]
+        if self.use_instr:
+            x = torch.cat((x, self.get_embed_instr(obs["instr"])), dim=1)
         x = self.c_fc1(x)
         x = F.tanh(x)
         x = self.c_fc2(x)
