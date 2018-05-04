285985d7e62ac05fc0139ff177cb8f1ec71e3692
Lucas Willems <lcswillems@gmail.com>
2018-05-04 08:55:45 -0400

    Make `__init__` method abstract

--- torch_ac/model.py
+++ torch_ac/model.py
@@ -4,10 +4,18 @@ import torch.nn.functional as F
 
 class ACModel(nn.Module):
     @abstractmethod
+    def __init__(self, obs_space, action_space):
+        pass
+        
+    @abstractmethod
     def forward(self, obs):
         pass
 
 class RecurrentACModel(nn.Module):
+    @abstractmethod
+    def __init__(self, obs_space, action_space):
+        pass
+
     @abstractmethod
     def forward(self, obs, state):
         pass
