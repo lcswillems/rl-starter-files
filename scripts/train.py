1ab69e2da34c648dfeb8a8285340ba49f617651c
Lucas Willems <lcswillems@gmail.com>
2018-05-21 18:27:21 -0400

    Add uppercase to Tensorboard

--- scripts/train.py
+++ scripts/train.py
@@ -35,7 +35,7 @@ parser.add_argument("--log-interval", type=int, default=1,
 parser.add_argument("--save-interval", type=int, default=0,
                     help="number of updates between two saves (default: 0, 0 means no saving)")
 parser.add_argument("--tb", action="store_true", default=False,
-                    help="log into tensorboard")
+                    help="log into Tensorboard")
 parser.add_argument("--frames-per-proc", type=int, default=None,
                     help="number of frames per process before update (default: 5 for A2C and 128 for PPO)")
 parser.add_argument("--discount", type=float, default=0.99,
@@ -106,7 +106,7 @@ elif args.algo == "ppo":
 else:
     raise ValueError
 
-# Define logger and tensorboard writer then
+# Define logger and Tensorboard writer then
 
 logger = utils.Logger(model_name)
 if args.tb:
