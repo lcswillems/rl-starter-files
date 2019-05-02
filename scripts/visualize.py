00e3c3eb44e2cb1477adfff395fac6b06ad8d992
mavroudisv <mavroudisv@gmail.com>
2019-05-02 08:45:54 +0100

    save visualization as gif

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -4,6 +4,9 @@ import argparse
 import gym
 import gym_minigrid
 import time
+import numpy as np
+from array2gif import write_gif
+
 
 import utils
 
@@ -22,6 +25,8 @@ parser.add_argument("--argmax", action="store_true", default=False,
                     help="select the action with highest probability")
 parser.add_argument("--pause", type=float, default=0.1,
                     help="pause duration between two consequent actions of the agent")
+parser.add_argument("--gif", type=str,
+                    help="store output as gif with the given filename")
 args = parser.parse_args()
 
 # Set seed for all randomness sources
@@ -42,6 +47,9 @@ agent = utils.Agent(args.env, env.observation_space, model_dir, args.argmax)
 
 # Run the agent
 
+if args.gif is not None:
+   frames = []
+
 done = True
 
 while True:
@@ -50,10 +58,16 @@ while True:
 
     time.sleep(args.pause)
     renderer = env.render()
+    if args.gif:
+        frames.append(np.moveaxis(env.render("rgb_array"), 2, 0))
 
     action = agent.get_action(obs)
     obs, reward, done, _ = env.step(action)
     agent.analyze_feedback(reward, done)
 
     if renderer.window is None:
+        if args.gif:
+            print("Saving gif... ",end='', flush=True)
+            write_gif(np.array(frames), args.gif+".gif", fps=10)
+            print("Done.")
         break
