ab16cba611150f6a5d88865133a7547f9f4d7618
Lucas Willems <lcswillems@gmail.com>
2019-12-31 00:51:46 +0100

    Remove MaxPool2d

--- model.py
+++ model.py
@@ -27,7 +27,6 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
         self.image_conv = nn.Sequential(
             nn.Conv2d(3, 16, (2, 2)),
             nn.ReLU(),
-            nn.MaxPool2d((2, 2)),
             nn.Conv2d(16, 32, (2, 2)),
             nn.ReLU(),
             nn.Conv2d(32, 64, (2, 2)),
@@ -35,7 +34,7 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
         )
         n = obs_space["image"][0]
         m = obs_space["image"][1]
-        self.image_embedding_size = ((n-1)//2-2)*((m-1)//2-2)*64
+        self.image_embedding_size = (n-3)*(m-3)*64
 
         # Define memory
         if self.use_memory:
