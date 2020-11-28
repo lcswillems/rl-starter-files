6190de39a4d00c3624c445ec62137fedb12dd7fa
Nathaniel Grabaskas <grabaskas@nathaniels-air.lan>
2020-11-27 16:34:18 -0800

    changes to work on my machine and printing l2-norm state changes per layer

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -5,109 +5,113 @@ from torch_ac.utils.penv import ParallelEnv
 
 import utils
 
+def main():
 
-# Parse arguments
+    # Parse arguments
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--env", required=True,
-                    help="name of the environment (REQUIRED)")
-parser.add_argument("--model", required=True,
-                    help="name of the trained model (REQUIRED)")
-parser.add_argument("--episodes", type=int, default=100,
-                    help="number of episodes of evaluation (default: 100)")
-parser.add_argument("--seed", type=int, default=0,
-                    help="random seed (default: 0)")
-parser.add_argument("--procs", type=int, default=16,
-                    help="number of processes (default: 16)")
-parser.add_argument("--argmax", action="store_true", default=False,
-                    help="action with highest probability is selected")
-parser.add_argument("--worst-episodes-to-show", type=int, default=10,
-                    help="how many worst episodes to show")
-parser.add_argument("--memory", action="store_true", default=False,
-                    help="add a LSTM to the model")
-parser.add_argument("--text", action="store_true", default=False,
-                    help="add a GRU to the model")
-args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", required=True,
+                        help="name of the environment (REQUIRED)")
+    parser.add_argument("--model", required=True,
+                        help="name of the trained model (REQUIRED)")
+    parser.add_argument("--episodes", type=int, default=100,
+                        help="number of episodes of evaluation (default: 100)")
+    parser.add_argument("--seed", type=int, default=0,
+                        help="random seed (default: 0)")
+    parser.add_argument("--procs", type=int, default=1,
+                        help="number of processes (default: 16)")
+    parser.add_argument("--argmax", action="store_true", default=False,
+                        help="action with highest probability is selected")
+    parser.add_argument("--worst-episodes-to-show", type=int, default=10,
+                        help="how many worst episodes to show")
+    parser.add_argument("--memory", action="store_true", default=False,
+                        help="add a LSTM to the model")
+    parser.add_argument("--text", action="store_true", default=False,
+                        help="add a GRU to the model")
+    args = parser.parse_args()
 
-# Set seed for all randomness sources
+    # Set seed for all randomness sources
 
-utils.seed(args.seed)
+    utils.seed(args.seed)
 
-# Set device
+    # Set device
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Device: {device}\n")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Device: {device}\n")
 
-# Load environments
+    # Load environments
 
-envs = []
-for i in range(args.procs):
-    env = utils.make_env(args.env, args.seed + 10000 * i)
-    envs.append(env)
-env = ParallelEnv(envs)
-print("Environments loaded\n")
+    envs = []
+    for i in range(args.procs):
+        env = utils.make_env(args.env, args.seed + 10000 * i)
+        envs.append(env)
+    env = ParallelEnv(envs)
+    print("Environments loaded\n")
 
-# Load agent
+    # Load agent
 
-model_dir = utils.get_model_dir(args.model)
-agent = utils.Agent(env.observation_space, env.action_space, model_dir,
-                    device=device, argmax=args.argmax, num_envs=args.procs,
-                    use_memory=args.memory, use_text=args.text)
-print("Agent loaded\n")
+    model_dir = utils.get_model_dir(args.model)
+    agent = utils.Agent(env.observation_space, env.action_space, model_dir,
+                        device=device, argmax=args.argmax, num_envs=args.procs,
+                        use_memory=args.memory, use_text=args.text)
+    print("Agent loaded\n")
 
-# Initialize logs
+    # Initialize logs
 
-logs = {"num_frames_per_episode": [], "return_per_episode": []}
+    logs = {"num_frames_per_episode": [], "return_per_episode": []}
 
-# Run agent
+    # Run agent
 
-start_time = time.time()
+    start_time = time.time()
 
-obss = env.reset()
+    obss = env.reset()
 
-log_done_counter = 0
-log_episode_return = torch.zeros(args.procs, device=device)
-log_episode_num_frames = torch.zeros(args.procs, device=device)
+    log_done_counter = 0
+    log_episode_return = torch.zeros(args.procs, device=device)
+    log_episode_num_frames = torch.zeros(args.procs, device=device)
 
-while log_done_counter < args.episodes:
-    actions = agent.get_actions(obss)
-    obss, rewards, dones, _ = env.step(actions)
-    agent.analyze_feedbacks(rewards, dones)
+    while log_done_counter < args.episodes:
+        actions = agent.get_actions(obss)
+        obss, rewards, dones, _ = env.step(actions)
+        agent.analyze_feedbacks(rewards, dones)
 
-    log_episode_return += torch.tensor(rewards, device=device, dtype=torch.float)
-    log_episode_num_frames += torch.ones(args.procs, device=device)
+        log_episode_return += torch.tensor(rewards, device=device, dtype=torch.float)
+        log_episode_num_frames += torch.ones(args.procs, device=device)
 
-    for i, done in enumerate(dones):
-        if done:
-            log_done_counter += 1
-            logs["return_per_episode"].append(log_episode_return[i].item())
-            logs["num_frames_per_episode"].append(log_episode_num_frames[i].item())
+        for i, done in enumerate(dones):
+            if done:
+                log_done_counter += 1
+                logs["return_per_episode"].append(log_episode_return[i].item())
+                logs["num_frames_per_episode"].append(log_episode_num_frames[i].item())
 
-    mask = 1 - torch.tensor(dones, device=device, dtype=torch.float)
-    log_episode_return *= mask
-    log_episode_num_frames *= mask
+        mask = 1 - torch.tensor(dones, device=device, dtype=torch.float)
+        log_episode_return *= mask
+        log_episode_num_frames *= mask
 
-end_time = time.time()
+    end_time = time.time()
 
-# Print logs
+    # Print logs
 
-num_frames = sum(logs["num_frames_per_episode"])
-fps = num_frames/(end_time - start_time)
-duration = int(end_time - start_time)
-return_per_episode = utils.synthesize(logs["return_per_episode"])
-num_frames_per_episode = utils.synthesize(logs["num_frames_per_episode"])
+    num_frames = sum(logs["num_frames_per_episode"])
+    fps = num_frames/(end_time - start_time)
+    duration = int(end_time - start_time)
+    return_per_episode = utils.synthesize(logs["return_per_episode"])
+    num_frames_per_episode = utils.synthesize(logs["num_frames_per_episode"])
 
-print("F {} | FPS {:.0f} | D {} | R:μσmM {:.2f} {:.2f} {:.2f} {:.2f} | F:μσmM {:.1f} {:.1f} {} {}"
-      .format(num_frames, fps, duration,
-              *return_per_episode.values(),
-              *num_frames_per_episode.values()))
+    print("F {} | FPS {:.0f} | D {} | R:μσmM {:.2f} {:.2f} {:.2f} {:.2f} | F:μσmM {:.1f} {:.1f} {} {}"
+          .format(num_frames, fps, duration,
+                  *return_per_episode.values(),
+                  *num_frames_per_episode.values()))
 
-# Print worst episodes
+    # Print worst episodes
 
-n = args.worst_episodes_to_show
-if n > 0:
-    print("\n{} worst episodes:".format(n))
+    n = args.worst_episodes_to_show
+    if n > 0:
+        print("\n{} worst episodes:".format(n))
 
-    indexes = sorted(range(len(logs["return_per_episode"])), key=lambda k: logs["return_per_episode"][k])
-    for i in indexes[:n]:
-        print("- episode {}: R={}, F={}".format(i, logs["return_per_episode"][i], logs["num_frames_per_episode"][i]))
+        indexes = sorted(range(len(logs["return_per_episode"])), key=lambda k: logs["return_per_episode"][k])
+        for i in indexes[:n]:
+            print("- episode {}: R={}, F={}".format(i, logs["return_per_episode"][i], logs["num_frames_per_episode"][i]))
+            
+if __name__ == '__main__':
+    main()
--- scripts/train.py
+++ scripts/train.py
@@ -1,6 +1,7 @@
 import argparse
 import time
 import datetime
+import numpy
 import torch
 import torch_ac
 import tensorboardX
@@ -9,196 +10,225 @@ import sys
 import utils
 from model import ACModel
 
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-# Parse arguments
 
-parser = argparse.ArgumentParser()
+def main():
+    # Parse arguments
 
-## General parameters
-parser.add_argument("--algo", required=True,
-                    help="algorithm to use: a2c | ppo (REQUIRED)")
-parser.add_argument("--env", required=True,
-                    help="name of the environment to train on (REQUIRED)")
-parser.add_argument("--model", default=None,
-                    help="name of the model (default: {ENV}_{ALGO}_{TIME})")
-parser.add_argument("--seed", type=int, default=1,
-                    help="random seed (default: 1)")
-parser.add_argument("--log-interval", type=int, default=1,
-                    help="number of updates between two logs (default: 1)")
-parser.add_argument("--save-interval", type=int, default=10,
-                    help="number of updates between two saves (default: 10, 0 means no saving)")
-parser.add_argument("--procs", type=int, default=16,
-                    help="number of processes (default: 16)")
-parser.add_argument("--frames", type=int, default=10**7,
-                    help="number of frames of training (default: 1e7)")
+    parser = argparse.ArgumentParser()
 
-## Parameters for main algorithm
-parser.add_argument("--epochs", type=int, default=4,
-                    help="number of epochs for PPO (default: 4)")
-parser.add_argument("--batch-size", type=int, default=256,
-                    help="batch size for PPO (default: 256)")
-parser.add_argument("--frames-per-proc", type=int, default=None,
-                    help="number of frames per process before update (default: 5 for A2C and 128 for PPO)")
-parser.add_argument("--discount", type=float, default=0.99,
-                    help="discount factor (default: 0.99)")
-parser.add_argument("--lr", type=float, default=0.001,
-                    help="learning rate (default: 0.001)")
-parser.add_argument("--gae-lambda", type=float, default=0.95,
-                    help="lambda coefficient in GAE formula (default: 0.95, 1 means no gae)")
-parser.add_argument("--entropy-coef", type=float, default=0.01,
-                    help="entropy term coefficient (default: 0.01)")
-parser.add_argument("--value-loss-coef", type=float, default=0.5,
-                    help="value loss term coefficient (default: 0.5)")
-parser.add_argument("--max-grad-norm", type=float, default=0.5,
-                    help="maximum norm of gradient (default: 0.5)")
-parser.add_argument("--optim-eps", type=float, default=1e-8,
-                    help="Adam and RMSprop optimizer epsilon (default: 1e-8)")
-parser.add_argument("--optim-alpha", type=float, default=0.99,
-                    help="RMSprop optimizer alpha (default: 0.99)")
-parser.add_argument("--clip-eps", type=float, default=0.2,
-                    help="clipping epsilon for PPO (default: 0.2)")
-parser.add_argument("--recurrence", type=int, default=1,
-                    help="number of time-steps gradient is backpropagated (default: 1). If > 1, a LSTM is added to the model to have memory.")
-parser.add_argument("--text", action="store_true", default=False,
-                    help="add a GRU to the model to handle text input")
+    ## General parameters
+    parser.add_argument("--algo", required=True,
+                        help="algorithm to use: a2c | ppo | ppo_intrinsic (REQUIRED)")
+    parser.add_argument("--env", required=True,
+                        help="name of the environment to train on (REQUIRED)")
+    parser.add_argument("--model", default=None,
+                        help="name of the model (default: {ENV}_{ALGO}_{TIME})")
+    parser.add_argument("--seed", type=int, default=1,
+                        help="random seed (default: 1)")
+    parser.add_argument("--log-interval", type=int, default=1,
+                        help="number of updates between two logs (default: 1)")
+    parser.add_argument("--save-interval", type=int, default=10,
+                        help="number of updates between two saves (default: 10, 0 means no saving)")
+    parser.add_argument("--procs", type=int, default=16,
+                        help="number of processes (default: 16)")
+    parser.add_argument("--frames", type=int, default=10**7,
+                        help="number of frames of training (default: 1e7)")
 
-args = parser.parse_args()
+    ## Parameters for main algorithm
+    parser.add_argument("--epochs", type=int, default=4,
+                        help="number of epochs for PPO (default: 4)")
+    parser.add_argument("--batch-size", type=int, default=256,
+                        help="batch size for PPO (default: 256)")
+    parser.add_argument("--frames-per-proc", type=int, default=None,
+                        help="number of frames per process before update (default: 5 for A2C and 128 for PPO)")
+    parser.add_argument("--discount", type=float, default=0.99,
+                        help="discount factor (default: 0.99)")
+    parser.add_argument("--lr", type=float, default=0.001,
+                        help="learning rate (default: 0.001)")
+    parser.add_argument("--gae-lambda", type=float, default=0.95,
+                        help="lambda coefficient in GAE formula (default: 0.95, 1 means no gae)")
+    parser.add_argument("--entropy-coef", type=float, default=0.01,
+                        help="entropy term coefficient (default: 0.01)")
+    parser.add_argument("--value-loss-coef", type=float, default=0.5,
+                        help="value loss term coefficient (default: 0.5)")
+    parser.add_argument("--max-grad-norm", type=float, default=0.5,
+                        help="maximum norm of gradient (default: 0.5)")
+    parser.add_argument("--optim-eps", type=float, default=1e-8,
+                        help="Adam and RMSprop optimizer epsilon (default: 1e-8)")
+    parser.add_argument("--optim-alpha", type=float, default=0.99,
+                        help="RMSprop optimizer alpha (default: 0.99)")
+    parser.add_argument("--clip-eps", type=float, default=0.2,
+                        help="clipping epsilon for PPO (default: 0.2)")
+    parser.add_argument("--recurrence", type=int, default=1,
+                        help="number of time-steps gradient is backpropagated (default: 1). If > 1, a LSTM is added to the model to have memory.")
+    parser.add_argument("--text", action="store_true", default=False,
+                        help="add a GRU to the model to handle text input")
 
-args.mem = args.recurrence > 1
+    args = parser.parse_args()
 
-# Set run dir
+    args.mem = args.recurrence > 1
 
-date = datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S")
-default_model_name = f"{args.env}_{args.algo}_seed{args.seed}_{date}"
+    # Set run dir
 
-model_name = args.model or default_model_name
-model_dir = utils.get_model_dir(model_name)
+    date = datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S")
+    default_model_name = f"{args.env}_{args.algo}_seed{args.seed}_{date}"
 
-# Load loggers and Tensorboard writer
+    model_name = args.model or default_model_name
+    model_dir = utils.get_model_dir(model_name)
 
-txt_logger = utils.get_txt_logger(model_dir)
-csv_file, csv_logger = utils.get_csv_logger(model_dir)
-tb_writer = tensorboardX.SummaryWriter(model_dir)
+    # Load loggers and Tensorboard writer
 
-# Log command and all script arguments
+    txt_logger = utils.get_txt_logger(model_dir)
+    csv_file, csv_logger = utils.get_csv_logger(model_dir)
+    tb_writer = tensorboardX.SummaryWriter(model_dir)
 
-txt_logger.info("{}\n".format(" ".join(sys.argv)))
-txt_logger.info("{}\n".format(args))
+    # Log command and all script arguments
 
-# Set seed for all randomness sources
+    txt_logger.info("{}\n".format(" ".join(sys.argv)))
+    txt_logger.info("{}\n".format(args))
 
-utils.seed(args.seed)
+    # Set seed for all randomness sources
 
-# Set device
+    utils.seed(args.seed)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-txt_logger.info(f"Device: {device}\n")
+    # Set device
 
-# Load environments
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    txt_logger.info(f"Device: {device}\n")
 
-envs = []
-for i in range(args.procs):
-    envs.append(utils.make_env(args.env, args.seed + 10000 * i))
-txt_logger.info("Environments loaded\n")
+    # Load environments
 
-# Load training status
+    envs = []
+    for i in range(args.procs):
+        envs.append(utils.make_env(args.env, args.seed + 10000 * i))
+    txt_logger.info("Environments loaded\n")
 
-try:
-    status = utils.get_status(model_dir)
-except OSError:
-    status = {"num_frames": 0, "update": 0}
-txt_logger.info("Training status loaded\n")
+    # Load training status
 
-# Load observations preprocessor
+    try:
+        status = utils.get_status(model_dir)
+    except OSError:
+        status = {"num_frames": 0, "update": 0}
+    txt_logger.info("Training status loaded\n")
 
-obs_space, preprocess_obss = utils.get_obss_preprocessor(envs[0].observation_space)
-if "vocab" in status:
-    preprocess_obss.vocab.load_vocab(status["vocab"])
-txt_logger.info("Observations preprocessor loaded")
+    # Load observations preprocessor
 
-# Load model
+    obs_space, preprocess_obss = utils.get_obss_preprocessor(envs[0].observation_space)
+    if "vocab" in status:
+        preprocess_obss.vocab.load_vocab(status["vocab"])
+    txt_logger.info("Observations preprocessor loaded")
 
-acmodel = ACModel(obs_space, envs[0].action_space, args.mem, args.text)
-if "model_state" in status:
-    acmodel.load_state_dict(status["model_state"])
-acmodel.to(device)
-txt_logger.info("Model loaded\n")
-txt_logger.info("{}\n".format(acmodel))
+    # Load model
 
-# Load algo
+    acmodel = ACModel(obs_space, envs[0].action_space, args.mem, args.text)
+    if "model_state" in status:
+        acmodel.load_state_dict(status["model_state"])
+    acmodel.to(device)
+    txt_logger.info("Model loaded\n")
+    txt_logger.info("{}\n".format(acmodel))
 
-if args.algo == "a2c":
-    algo = torch_ac.A2CAlgo(envs, acmodel, device, args.frames_per_proc, args.discount, args.lr, args.gae_lambda,
-                            args.entropy_coef, args.value_loss_coef, args.max_grad_norm, args.recurrence,
-                            args.optim_alpha, args.optim_eps, preprocess_obss)
-elif args.algo == "ppo":
-    algo = torch_ac.PPOAlgo(envs, acmodel, device, args.frames_per_proc, args.discount, args.lr, args.gae_lambda,
-                            args.entropy_coef, args.value_loss_coef, args.max_grad_norm, args.recurrence,
-                            args.optim_eps, args.clip_eps, args.epochs, args.batch_size, preprocess_obss)
-else:
-    raise ValueError("Incorrect algorithm name: {}".format(args.algo))
+    # Load algo
 
-if "optimizer_state" in status:
-    algo.optimizer.load_state_dict(status["optimizer_state"])
-txt_logger.info("Optimizer loaded\n")
+    if args.algo == "a2c":
+        algo = torch_ac.A2CAlgo(envs, acmodel, device, args.frames_per_proc, args.discount, args.lr, args.gae_lambda,
+                                args.entropy_coef, args.value_loss_coef, args.max_grad_norm, args.recurrence,
+                                args.optim_alpha, args.optim_eps, preprocess_obss)
+    elif args.algo == "ppo":
+        algo = torch_ac.PPOAlgo(envs, acmodel, device, args.frames_per_proc, args.discount, args.lr, args.gae_lambda,
+                                args.entropy_coef, args.value_loss_coef, args.max_grad_norm, args.recurrence,
+                                args.optim_eps, args.clip_eps, args.epochs, args.batch_size, preprocess_obss)
+                                
+    elif args.algo == "ppo_intrinsic":
+        algo = torch_ac.PPOAlgoIntrinsic(envs, acmodel, device, args.frames_per_proc, args.discount, args.lr, args.gae_lambda,
+                                args.entropy_coef, args.value_loss_coef, args.max_grad_norm, args.recurrence,
+                                args.optim_eps, args.clip_eps, args.epochs, args.batch_size, preprocess_obss)
+    else:
+        raise ValueError("Incorrect algorithm name: {}".format(args.algo))
 
-# Train model
+    if "optimizer_state" in status:
+        algo.optimizer.load_state_dict(status["optimizer_state"])
+    txt_logger.info("Optimizer loaded\n")
 
-num_frames = status["num_frames"]
-update = status["update"]
-start_time = time.time()
+    # Train model
 
-while num_frames < args.frames:
-    # Update model parameters
+    num_frames = status["num_frames"]
+    update = status["update"]
+    start_time = time.time()
 
-    update_start_time = time.time()
-    exps, logs1 = algo.collect_experiences()
-    logs2 = algo.update_parameters(exps)
-    logs = {**logs1, **logs2}
-    update_end_time = time.time()
+    while num_frames < args.frames:
+#        old_parameters = {}
+#        for name, param in acmodel.named_parameters():
+#            old_parameters[name] = param
+            
+        # Update model parameters
 
-    num_frames += logs["num_frames"]
-    update += 1
+        update_start_time = time.time()
+        exps, logs1 = algo.collect_experiences()
+        logs2 = algo.update_parameters(exps)
+        logs = {**logs1, **logs2}
+        update_end_time = time.time()
 
-    # Print logs
+        num_frames += logs["num_frames"]
+        update += 1
 
-    if update % args.log_interval == 0:
-        fps = logs["num_frames"]/(update_end_time - update_start_time)
-        duration = int(time.time() - start_time)
-        return_per_episode = utils.synthesize(logs["return_per_episode"])
-        rreturn_per_episode = utils.synthesize(logs["reshaped_return_per_episode"])
-        num_frames_per_episode = utils.synthesize(logs["num_frames_per_episode"])
+        # Print logs
 
-        header = ["update", "frames", "FPS", "duration"]
-        data = [update, num_frames, fps, duration]
-        header += ["rreturn_" + key for key in rreturn_per_episode.keys()]
-        data += rreturn_per_episode.values()
-        header += ["num_frames_" + key for key in num_frames_per_episode.keys()]
-        data += num_frames_per_episode.values()
-        header += ["entropy", "value", "policy_loss", "value_loss", "grad_norm"]
-        data += [logs["entropy"], logs["value"], logs["policy_loss"], logs["value_loss"], logs["grad_norm"]]
+        if update % args.log_interval == 0:
+            fps = logs["num_frames"]/(update_end_time - update_start_time)
+            duration = int(time.time() - start_time)
+            return_per_episode = utils.synthesize(logs["return_per_episode"])
+            rreturn_per_episode = utils.synthesize(logs["reshaped_return_per_episode"])
+            num_frames_per_episode = utils.synthesize(logs["num_frames_per_episode"])
 
-        txt_logger.info(
-            "U {} | F {:06} | FPS {:04.0f} | D {} | rR:μσmM {:.2f} {:.2f} {:.2f} {:.2f} | F:μσmM {:.1f} {:.1f} {} {} | H {:.3f} | V {:.3f} | pL {:.3f} | vL {:.3f} | ∇ {:.3f}"
-            .format(*data))
+            header = ["update", "frames", "FPS", "duration"]
+            data = [update, num_frames, fps, duration]
+            header += ["rreturn_" + key for key in rreturn_per_episode.keys()]
+            data += rreturn_per_episode.values()
+            header += ["num_frames_" + key for key in num_frames_per_episode.keys()]
+            data += num_frames_per_episode.values()
+            header += ["entropy", "value", "policy_loss", "value_loss", "grad_norm"]
+            data += [logs["entropy"], logs["value"], logs["policy_loss"], logs["value_loss"], logs["grad_norm"]]
 
-        header += ["return_" + key for key in return_per_episode.keys()]
-        data += return_per_episode.values()
+            txt_logger.info(
+                "U {} | F {:06} | FPS {:04.0f} | D {} | rR:μσmM {:.2f} {:.2f} {:.2f} {:.2f} | F:μσmM {:.1f} {:.1f} {} {} | H {:.3f} | V {:.3f} | pL {:.3f} | vL {:.3f} | ∇ {:.3f}"
+                .format(*data))
 
-        if status["num_frames"] == 0:
-            csv_logger.writerow(header)
-        csv_logger.writerow(data)
-        csv_file.flush()
+            header += ["return_" + key for key in return_per_episode.keys()]
+            data += return_per_episode.values()
 
-        for field, value in zip(header, data):
-            tb_writer.add_scalar(field, value, num_frames)
+            if status["num_frames"] == 0:
+                csv_logger.writerow(header)
+            csv_logger.writerow(data)
+            csv_file.flush()
 
-    # Save status
+            for field, value in zip(header, data):
+                tb_writer.add_scalar(field, value, num_frames)
 
-    if args.save_interval > 0 and update % args.save_interval == 0:
-        status = {"num_frames": num_frames, "update": update,
-                  "model_state": acmodel.state_dict(), "optimizer_state": algo.optimizer.state_dict()}
-        if hasattr(preprocess_obss, "vocab"):
-            status["vocab"] = preprocess_obss.vocab.vocab
-        utils.save_status(status, model_dir)
-        txt_logger.info("Status saved")
+        # Save status
+
+        if args.save_interval > 0 and update % args.save_interval == 0:
+            status = {"num_frames": num_frames, "update": update,
+                      "model_state": acmodel.state_dict(), "optimizer_state": algo.optimizer.state_dict()}
+            if hasattr(preprocess_obss, "vocab"):
+                status["vocab"] = preprocess_obss.vocab.vocab
+            utils.save_status(status, model_dir)
+            txt_logger.info("Status saved")
+            
+#        new_parameters = {}
+#        for name, param in acmodel.named_parameters():
+#            new_parameters[name] = param
+#
+##        for index in range(len(old_parameters.keys())):
+#        for index in range(1):
+#            key = list(old_parameters.keys())[index]
+#            old_weights = old_parameters[key].detach().numpy()
+#            new_weights = new_parameters[key].detach().numpy()
+#            print("Old Norm", numpy.linalg.norm(old_weights))
+#            print("New Norm", numpy.linalg.norm(new_weights))
+#            print(key + " diff       \t", numpy.sum(old_weights - new_weights))
+
+if __name__ == '__main__':
+    main()
