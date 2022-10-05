a56739fd9b7a3c5f87abb75fb06c56112c148b60
Lucas Willems <lcswillems@gmail.com>
2022-10-05 19:40:49 +0200

    Simplify make_env

--- utils/env.py
+++ utils/env.py
@@ -2,9 +2,6 @@ import gymnasium as gym
 
 
 def make_env(env_key, seed=None, render_mode=None):
-    if render_mode is None:
-        env = gym.make(env_key)
-    else:
-        env = gym.make(env_key, render_mode=render_mode)
+    env = gym.make(env_key, render_mode=render_mode)
     env.reset(seed=seed)
     return env
